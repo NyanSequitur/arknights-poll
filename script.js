e030a9a285d5c1eaf2af4c18bb584b877c1a54d1
NyanSeqiutur <nyansequiturs@gmail.com>
2025-02-21 08:03:39 +0100

    i dont know what im doinggggg i hate the internetttt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, update, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 // Firebase Configuration
 const firebaseConfig = {
